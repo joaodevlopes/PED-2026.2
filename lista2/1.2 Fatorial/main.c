1ed6fb710465007da203f874f2d74391302723d7
João Gabriel <joaodevlopes@gmail.com>
2026-08-27 09:55:44 -0300

    Add files via upload

---
+++ lista2/1.2 Fatorial/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// PILHA:
+
+//fatorial(1) return 1
+//fatorial(2) return 2 * fatorial(1) = 2 * 1
+//fatorial(3) return 3 * fatorial(2) = 3 * 2
+//fatorial(4) return 4 * fatorial(3 ) = 4 * 6
+//fatorial(5) return 5 * fatorial(4) = 5 * 24
+//fatorial(6) return 6 * fatorial(5) = 6 * 120
+//fatorial(7) return 7 * fatorial(6) = 7 * 720 = 5040
+
+
+int fatorial(int n){
+    if(n == 1) return 1;
+    else return n * fatorial(n - 1);
+}
+
+int main(){
+    printf("%d", fatorial(7));
+    return 0;
+}
